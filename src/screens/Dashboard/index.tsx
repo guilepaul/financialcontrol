88a445800d41f75bc378db4377b4a2126f6a0560
Guilherme Di Paula <guilepaul@gmail.com>
2021-11-14 22:45:02 -0300

    global theme

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import * as S from './styles'
+
+export function Dashboard(){
+    return(
+        <S.Container>
+            <S.Title>Dashboard</S.Title>
+        </S.Container>
+    )
+}
+
