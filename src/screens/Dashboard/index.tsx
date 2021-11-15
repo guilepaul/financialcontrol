8c8b7647a6b9b857bcee316a68a069b32e6093c5
Guilherme Di Paula <guilepaul@gmail.com>
2021-11-14 23:14:02 -0300

    User Header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -5,7 +5,17 @@ import * as S from './styles'
 export function Dashboard(){
     return(
         <S.Container>
-            <S.Header />
+            <S.Header >
+                <S.UserWrapper>
+                    <S.UserInfo>
+                        <S.Photo source={{uri: 'https://avatars.githubusercontent.com/u/44080230?v=4' }} />
+                        <S.User>
+                            <S.UserGreeting>Olá, </S.UserGreeting>
+                            <S.UserName>Guilherme</S.UserName>
+                        </S.User>
+                    </S.UserInfo>
+                </S.UserWrapper>
+            </S.Header>
         </S.Container>
     )
 }
