540c785b6249af64d8db1a55714e6e03994e0904
Guilherme Di Paula <guilepaul@gmail.com>
2021-11-14 22:57:22 -0300

    RFPercentage

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -5,7 +5,7 @@ import * as S from './styles'
 export function Dashboard(){
     return(
         <S.Container>
-            <S.Title>Dashboard</S.Title>
+            <S.Header />
         </S.Container>
     )
 }
