cfd026d49ae99cc63e2f2a87e144bbaa25e341d9
Guilherme Di Paula <guilepaul@gmail.com>
2021-11-15 00:29:36 -0300

    highlight-cards done

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import * as S from './styles'
-import HighlightCard from '../../components/HighlightCard';
+import {HighlightCard} from '../../components/HighlightCard';
 
 export function Dashboard(){
     return(
@@ -21,9 +21,24 @@ export function Dashboard(){
             <S.HighLightCards 
                 
                 >
-                <HighlightCard />
-                <HighlightCard />
-                <HighlightCard />
+                <HighlightCard 
+                title= 'Entradas'
+                amount="R$ 17.400,00"
+                lastTransaction='Última entrada dia 12 de abril'
+                type= 'up'
+                />
+                <HighlightCard 
+                title= 'Saídas'
+                amount="R$ 1.259,00"
+                lastTransaction='Última saída dia 03 de abril'
+                type='down'
+                />
+                <HighlightCard 
+                title= 'Total'
+                amount="R$ 16.141,00"
+                lastTransaction='01 à 16 de abril'
+                type='total'
+                />
             </S.HighLightCards>
         </S.Container>
     )
