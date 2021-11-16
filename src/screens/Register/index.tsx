a99fb87e6b4b24a1b62c42c5d609e5f1c0c68b08
Guilherme Di Paula <guilepaul@gmail.com>
2021-11-15 22:19:51 -0300

    inputs and button

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import {Input} from '../../components/Form/Input'
+import {Button} from '../../components/Form/Button'
+
+import * as S from './styles';
+
+export function Register() {
+  return (
+      <S.Container>
+          <S.Header>
+              <S.Title>Cadastro</S.Title>
+          </S.Header>
+          <S.Form>
+            <S.Fields>
+                <Input
+                    placeholder="Nome"
+                    />
+                <Input
+                    placeholder="Preço"
+                    />
+            </S.Fields>
+            <Button title="Enviar" />
+        </S.Form>
+      </S.Container>
+  );
+}
