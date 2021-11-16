3333b0fc2a3a08d4bf9a2f8a6bfe44cc118385bc
Guilherme Di Paula <guilepaul@gmail.com>
2021-11-15 23:57:48 -0300

    category list

--- App.tsx
+++ App.tsx
@@ -12,6 +12,7 @@ import {
 import theme from './src/global/styles/theme'
 import { Dashboard } from './src/screens/Dashboard';
 import { Register } from './src/screens/Register';
+import { CategorySelect } from './src/screens/CategorySelect';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,8 +26,9 @@ export default function App() {
   }
   return(
     <ThemeProvider theme={theme} >
-      <Register />
+      {/* <Register /> */}
       {/* <Dashboard /> */}
+      <CategorySelect />
     </ThemeProvider>
     )
     
